f1150f92ce37720f7227ff1483a4cf60c088168d
Dmitriy Krayniy <dkrayniy@entinco.com>
2022-12-21 08:51:31 +0400

    Initial commit

---
+++ checkout.ps1
@@ -0,0 +1,6 @@
+#!/usr/bin/env pwsh
+
+git clone git@github.com:pip-services-support2/service-announcements-node.git
+git clone git@github.com:pip-services-support2/service-feedbacks-node.git
+git clone git@github.com:pip-services-support2/client-feedbacks-node.git
+git clone git@github.com:pip-services-support2/client-announcements-node.git
